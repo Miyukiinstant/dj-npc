34d026c361f430275d9389f4ddc3ecec6796c16a
Miyuki <84234307+Miyukiinstant@users.noreply.github.com>
2021-09-27 23:19:54 +0200

    DELETE
    Removed cl_dj.lua
    Removed the npc_track

--- lua/ulx/modules/sh/npc_jukebox.lua
+++ lua/ulx/modules/sh/npc_jukebox.lua
@@ -1,29 +1,11 @@
-function ulx.npc_jukebox( calling_ply)
-    local npc_jukebox = "npc_jukebox"
-    local track_jukebox = "track_jukebox"
-    local track2_jukebox = "track2_jukebox"
-    if calling_ply:GetInfoNum(npc_jukebox, "1") > 0 then
-        calling_ply:ConCommand("stopsound")
-        calling_ply:ConCommand(npc_jukebox .. " 0")
-    else
-        calling_ply:ConCommand(track2_jukebox .. " 0")
-        calling_ply:ConCommand(track_jukebox.." 0")
-        calling_ply:ConCommand(npc_jukebox.." 1")        
-    end
-end
-local npc_jukebox = ulx.command( "Fun", "ulx npcjukebox", ulx.npc_jukebox, "!npcjukebox" )
-npc_jukebox:defaultAccess( ULib.DEFAULT_ACCESS )
-
 function ulx.track_jukebox( calling_ply)
     local track_jukebox = "track_jukebox"
     local track2_jukebox = "track2_jukebox"
-    local npc_jukebox = "npc_jukebox"
     if calling_ply:GetInfoNum(track_jukebox, "1") > 0 then
         calling_ply:ConCommand("stopsound")
         calling_ply:ConCommand(track_jukebox .." 0")
     else
         calling_ply:ConCommand(track2_jukebox .. " 0")
-        calling_ply:ConCommand(npc_jukebox.." 0")
         calling_ply:ConCommand(track_jukebox.." 1")
     end
 end
@@ -31,7 +13,6 @@ local track_jukebox = ulx.command( "Fun", "ulx track01", ulx.track_jukebox, "!tr
 track_jukebox:defaultAccess( ULib.DEFAULT_ACCESS )
 
 function ulx.track2_jukebox( calling_ply)
-    local npc_jukebox = "npc_jukebox"
     local track2_jukebox = "track2_jukebox"
     local track_jukebox = "track_jukebox"
     if calling_ply:GetInfoNum(track2_jukebox, "1") > 0 then
@@ -39,8 +20,7 @@ function ulx.track2_jukebox( calling_ply)
         calling_ply:ConCommand(track2_jukebox .. " 0")
     else
         calling_ply:ConCommand(track2_jukebox.." 1")
-        calling_ply:ConCommand(track_jukebox.." 0")      
-        calling_ply:ConCommand(npc_jukebox.." 0")     
+        calling_ply:ConCommand(track_jukebox.." 0")        
     end
 end
 local track2_jukebox = ulx.command( "Fun", "ulx track02", ulx.track2_jukebox, "!track02" )
